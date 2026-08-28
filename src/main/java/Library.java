a1bc37219becb255f154da771b4fa28c21d6ff70
Johnson Laguerre <johnsonlaguerre@outlook.com>
2026-08-27 22:12:35 -0400

    Work on read, update entries.

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -187,8 +187,18 @@ public class Library {
 
     public void printSearchResults(){
         ArrayList<Entry> searchResults = this.getSearchResults();
+        this.printEntries(searchResults);
+    }
 
-        for (Entry entry : searchResults){
+    public void printEntries(ArrayList<Entry> entries){
+        int size = entries.size();
+
+        if (size <= 0){
+            System.out.println("0 entries to print.");
+            return;
+        }
+
+        for (Entry entry : entries){
             System.out.println(
                     "Entry ID: " + entry.getEntryId() + "\n" +
                     "URL: "      + entry.getEntryUrl()     + "\n" +
@@ -241,21 +251,11 @@ public class Library {
         return manyResults;
     }
 
-    public void printManyEntries(){
-        ArrayList<Entry> manyEntries = this.getManyEntries();
-
-        for (Entry entry : manyEntries){
-            System.out.println(
-                    "Entry ID: " + entry.getEntryId() + "\n" +
-                            "URL: "      + entry.getEntryUrl()     + "\n" +
-                            "Title: "    + entry.getEntryTitle()   + "\n" +
-//                    "Content: "  + entry.getEntryBody() + "\n" +
-                            "Notes: "    + entry.getEntryNotes()   + "\n\n"
-            );
-        }
+    public void printEntry(Entry entry){
+        this.readEntry(entry);
     }
 
-    public static void readEntry(Entry entry){
+    public void readEntry(Entry entry){
 		System.out.println("\nEntry ID: " + entry.getEntryId());
 		System.out.println("Entry title: " + entry.getEntryTitle());
 		System.out.println("Entry URL: " + entry.getEntryUrl());
@@ -263,10 +263,18 @@ public class Library {
 		System.out.println("\nEntry notes:\n\n" + entry.getEntryNotes());
 	}
 
+    public void readEntryMinusBody(Entry entry){
+		System.out.println("\nEntry ID: " + entry.getEntryId());
+		System.out.println("Entry title: " + entry.getEntryTitle());
+		System.out.println("Entry URL: " + entry.getEntryUrl());
+		System.out.println("\nEntry notes:\n\n" + entry.getEntryNotes());
+	}
+
     public Entry getEntry(int entryId){
         Connection conn = this.getConnection();
         Entry entry = new Entry();
 
+        System.out.println("Getting entry " + entryId);
         try {
 
             tableName = this.getTableName() + " ";
@@ -293,14 +301,70 @@ public class Library {
             entry.setEntryNotes(entryNotes);
 
         } catch (Exception e){
-            throw new RuntimeException(e);
+            System.out.println(e);
+//            throw new RuntimeException(e);
         }
 
         return entry;
     }
 
+    // Update entry in database.
     public void setEntry(int entryId){
+        Scanner scanner = new Scanner(System.in);
+        Entry entry = this.getEntry(entryId);
+        String answer = "";
+        PreparedStatement ps = null;
+        Connection conn = this.getConnection();
 
+        System.out.println("Current entry:\n");
+        this.readEntryMinusBody(entry);
+
+        System.out.println("Editable fields: URL, title, notes.\n");
+
+        System.out.println("Updated entry URL: " + entry.getEntryUrl());
+        String entryUrl = scanner.nextLine();
+
+        System.out.println("Updated entry title: " + entry.getEntryTitle());
+        String entryTitle = scanner.nextLine();
+
+        System.out.println("Updated entry notes:\n" + entry.getEntryNotes());
+        String entryNotes = scanner.nextLine();
+
+        System.out.println("After modifications:\n");
+        System.out.println("Entry URL: " + entryUrl);
+        System.out.println("Entry title: " + entryTitle);
+        System.out.println("Entry notes:\n" + entryNotes);
+
+        do {
+            System.out.println("Save changes? Y/y for yes, N/n for no: ");
+            answer = scanner.nextLine();
+
+            try {
+                if (answer.equalsIgnoreCase("y")){
+                    String tableName = this.getTableName() + " ";
+                    String line = "UPDATE " + tableName + "SET entry_url = ?, entry_title = ?, entry_notes = ? WHERE entry_id = ?";
+                    ps = conn.prepareStatement(line);
+                    ps.setString(1, entryUrl);
+                    ps.setString(2, entryTitle);
+                    ps.setString(3, entryNotes);
+                    ps.setInt(4, entryId);
+
+                    int result = ps.executeUpdate();
+
+                    if (result == 0){
+                        System.out.println("Could not execute UPDATE for entry " + entryId + ".");
+                    } else if (result == 1){
+                        System.out.println("Successful update for entry " + entryId + ".");
+                    }
+                } else if (answer.equalsIgnoreCase("n")){
+                    System.out.println("Entry will not be updated.");
+                    return;
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+
+        } while (!answer.equalsIgnoreCase("y") || !answer.equalsIgnoreCase("n"));
 
     }
 
@@ -340,12 +404,12 @@ public class Library {
                 throw new RuntimeException(e);
             }
 
-        } while (answer.equalsIgnoreCase("y") || answer.equalsIgnoreCase("n"));
+        } while (!answer.equalsIgnoreCase("y") || !answer.equalsIgnoreCase("n"));
 
         scanner.close();
     }
 
-    public static void parseCreate(String line, String[] arr, HashMap<String, String> hashMap) {
+    public void parseCreate(String line, String[] arr, HashMap<String, String> hashMap) {
         String keyword;
 		String defaultInfileName = "links.txt";
         int i = 0, len = arr.length;
@@ -382,7 +446,7 @@ public class Library {
             return "unrecognized";
         }
     }
-	
+
 	public void testConnection(){
 		System.out.println("Testing database connection. Latest entry id: " + this.getLatestEntryId());
 	}
@@ -391,43 +455,64 @@ public class Library {
         String firstWord = arr[0].toLowerCase();
 
         switch (firstWord) {
-            case "create":
-            case "c":
-                HashMap<String, String> createHashMap = new HashMap<>();
-                Library.parseCreate(line, arr, createHashMap);
-                this.createEntries(createHashMap);
-                break;
             case "q":
                 break;
-            case "get":
-            case "read":
+            case "c":
+            case "create":
+                HashMap<String, String> createHashMap = new HashMap<>();
+                this.parseCreate(line, arr, createHashMap);
+                this.createEntries(createHashMap);
+                break;
             case "r":
+            case "read":
             case "view":
+            case "get":
                 String readResult = Library.parseRead(line, arr);
-
                 if (readResult.equalsIgnoreCase("search-results")) {
                     this.printSearchResults();
                 } else if (readResult.equalsIgnoreCase("unrecognized")) {
                     System.out.println("Unrecognized read option: " + arr[1]);
-                } else if (readResult.equalsIgnoreCase("all")) {
-                    this.queryManyEntries();
-                    this.printManyEntries();
+                } else if (readResult.equalsIgnoreCase("many")) {
+                    ArrayList<Entry> manyEntries = this.queryManyEntries();
+                    this.printEntries(manyEntries);
                 } else {
                     int readEntryId = Integer.parseInt(readResult);
                     Entry readEntry = this.getEntry(readEntryId);
-                    Library.readEntry(readEntry);
+                    this.readEntry(readEntry);
                 }
                 break;
-            case "testfetch":
-            case "tfetch":
-                Utilities.testFetch();
+            case "u":
+            case "update":
+            case "s":
+            case "set":
+                String updateResult = Library.parseRead(line, arr);
+                try {
+                    int updateEntryId = Integer.parseInt(updateResult);
+                    this.updateEntry(updateEntryId);
+                } catch (NumberFormatException e) {
+                    System.out.println("Unrecognized update option: " + updateResult);
+                }
                 break;
-            case "latest-entry-id":
-            case "latestid":
-            case "lid":
-                int id = this.getLatestEntryId();
-                System.out.println("Latest entry id: " + id);
+            case "d":
+            case "delete":
+                String deleteResult = Library.parseRead(line, arr);
+                try {
+                    int deleteEntryId = Integer.parseInt(deleteResult);
+                    this.deleteEntry(deleteEntryId);
+                } catch (NumberFormatException e) {
+                    System.out.println("Unrecognized delete option: " + deleteResult);
+                }
                 break;
+//            case "tfetch":
+//            case "testfetch":
+//                Utilities.testFetch();
+//                break;
+//            case "lid":
+//            case "latestid":
+//            case "latest-entry-id":
+//                int id = this.getLatestEntryId();
+//                System.out.println("Latest entry id: " + id);
+//                break;
             default:
                 break;
         }
